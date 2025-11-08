baec7753175cf21ea28ec1142bc90cc881b437d8
zaxnathanson <zaxliyen@gmail.com>
2025-11-07 23:33:48 -0500

    Apples fall

--- ChampJamApples/Assets/Apple.cs
+++ ChampJamApples/Assets/Apple.cs
@@ -3,66 +3,114 @@ using UnityEngine;
 public class Apple : MonoBehaviour
 {
     [HideInInspector] public bool doesHaveWorm;
-    [HideInInspector] public bool isAttached;
+    [HideInInspector] public bool isAttached = true;
     PlayerMovement wormRef;
     GameManager manager;
 
     Vector2 dragDistance;
     [SerializeField] float maxDragDistance;
     [SerializeField] float launchMultiplier;
+    [SerializeField] int health;
+    [SerializeField] float fallSpeed;
 
     [Header("Arrow")]
     [SerializeField] GameObject arrow;
     [SerializeField] float maxArrowLength;
     [SerializeField] float minArrowLength;
 
+    enum ShakeDirections
+    {
+        NONE,
+        LEFT,
+        RIGHT,
+    }
+    ShakeDirections lastShakeDirection;
+
     void Start()
     {
         manager = GameManager.instance;
         wormRef = manager.playerRef;
+        isAttached = true;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Does Have Worm
         if (doesHaveWorm)
         {
-            if (Input.GetMouseButton(0))
-            {
-                Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                dragDistance = new Vector2(transform.position.x, transform.position.y) - mousePos;
-                dragDistance = Vector2.ClampMagnitude(dragDistance, maxDragDistance);
-
-                //Arrow
-                arrow.SetActive(true);
-
-                Vector2 direction = dragDistance.normalized;
-                float arrowRotation = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90;
-                arrow.transform.eulerAngles = new Vector3(0, 0, arrowRotation);
-
-                Vector3 minScale = new Vector3(arrow.transform.localScale.x, minArrowLength, 0);
-                Vector3 maxScale = new Vector3(arrow.transform.localScale.x, maxArrowLength, 0);
-                arrow.transform.localScale = Vector3.Lerp(minScale, maxScale, dragDistance.magnitude/maxDragDistance);
-            }
-            else
-            {
-                arrow.SetActive(false);
-            }
-
-            if (Input.GetMouseButtonUp(0))
-            {
-                WormLaunch();
-            }
+            WormShake();
+            DetectLaunch();
         }
         else
         {
             arrow.SetActive(false);
         }
+
+        DropApple();
     }
+
+    void DropApple()
+    {
+        if (health <= 0 && isAttached)
+        {
+            Debug.Log("Drop");
+            //Drop Apple Effects
+            isAttached = false;
+        }
+
+        if (!isAttached)
+        {
+            transform.position -= new Vector3(0, fallSpeed, 0) * Time.deltaTime;
+        }
+    }
+
+    void DetectLaunch()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            dragDistance = new Vector2(transform.position.x, transform.position.y) - mousePos;
+            dragDistance = Vector2.ClampMagnitude(dragDistance, maxDragDistance);
+
+            //Arrow
+            arrow.SetActive(true);
+
+            Vector2 direction = dragDistance.normalized;
+            float arrowRotation = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90;
+            arrow.transform.eulerAngles = new Vector3(0, 0, arrowRotation);
+
+            Vector3 minScale = new Vector3(arrow.transform.localScale.x, minArrowLength, 0);
+            Vector3 maxScale = new Vector3(arrow.transform.localScale.x, maxArrowLength, 0);
+            arrow.transform.localScale = Vector3.Lerp(minScale, maxScale, dragDistance.magnitude / maxDragDistance);
+        }
+        else
+        {
+            arrow.SetActive(false);
+        }
+
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            WormLaunch();
+        }
+    }
+
+
 
     void WormShake()
     {
+        if (Input.GetAxisRaw("Horizontal") > 0 && lastShakeDirection != ShakeDirections.RIGHT)
+        {
+            lastShakeDirection = ShakeDirections.RIGHT;
+            health--;
+        }
 
+        if (Input.GetAxisRaw("Horizontal") < 0 && lastShakeDirection != ShakeDirections.LEFT)
+        {
+            lastShakeDirection = ShakeDirections.LEFT;
+            health--;
+        }
     }
 
     void WormLaunch()
